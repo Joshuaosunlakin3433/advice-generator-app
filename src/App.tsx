215d5bb4db22bdb6442bb966d5736a2cab84aaf2
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-05-08 14:23:43 +0100

    style: enhance UI elements and improve typography

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,15 @@ const App = () => {
 
   return (
     <main className="bg-[#222732] h-screen w-screen flex items-center justify-center">
-      <div className="relative bg-[#323a49] h-60 w-96 flex flex-col items-center justify-center px-3 gap-5 rounded-lg">
-        <p className="text-[#52ffaa]">#Advice #{id}</p>
-        <blockquote className="text-center text-white">{advice}</blockquote>
+      <div className="relative bg-[#323a49] h-60 w-96 flex flex-col items-center justify-center px-3 gap-5 rounded-lg pb-3">
+        <p className="text-[#52ffaa] uppercase font-semibold tracking-widest font-primary text-sm">Advice #{id}</p>
+        <q className="text-center text-[hsl(193,38%,86%)] font-primary text-lg pb-3">{advice}</q>
 
-        <div className="relative w-full">
-          <hr className="text-white w-full absolute" />
-          <TiMediaPause className="absolute bg-[#323a49] rounded-full text-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+        <div className="relative w-full"> 
+          <hr className="text-white w-full absolute border-1 border-[hsl(217,19%,38%)]"/>
+          <TiMediaPause className="absolute bg-[#323a49] text-white/60 rounded-full text-3xl top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
         </div>
-        <div className="absolute -bottom-5 size-12 bg-[#52ffaa] rounded-full flex items-center justify-center">
+        <div className="absolute ml-2 -bottom-5 size-12 bg-[#52ffaa] rounded-full flex items-center justify-center">
           <LuDices className="text-4xl p-1.5 text-white animate-bounce transition duration-500 cursor-pointer" onClick={()=> fetchAdvice()}/>
         </div>
       </div>
